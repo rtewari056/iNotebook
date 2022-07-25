df2b5da782f4da3b0469edc514953a2b621094df
Rohit Tewari <rtewari056@gmail.com>
2022-07-25 09:47:36 +0530

    iNotebook db & Express Server Setup

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo = async () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to mongoose successfully");
+  });
+};
+
+module.exports = connectToMongo;
