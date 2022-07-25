0a22650a75940ebe651873c8a4d98c03eaef88ee
Rohit Tewari <rtewari056@gmail.com>
2022-07-25 23:21:09 +0530

    Creating ThunderClient Collections to Manage Requests

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000 // As port 3000 occupied by React App
 
 app.use(express.json());
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-// Create a User using: POST "/api/auth". Doesn't require auth
+// Create a User using: POST "/api/auth/createuser". No login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
@@ -13,16 +13,34 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    }).then((user) => res.json(user)).catch((err) => res.json({error: 'Please enter a unique value for email'}));
+
+    // Check whether the email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry! User with this email already exists" });
+      }
+
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      res.json(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 
