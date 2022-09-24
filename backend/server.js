cab92005c613214369e924d70b2120938a1c29fa
Rohit Tewari <rtewari056@gmail.com>
2022-09-24 09:27:45 +0530

    Added API Routes

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,46 @@ const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const app = express();
 dotenv.config();
 connectToMongo();
-const port = process.env.PORT || 5000; // As port 3000 occupied by React App
+const PORT = process.env.PORT || 5000; // As port 3000 occupied by React App
 
 app.use(express.json());
 app.use(cors());
+
+// API home page for all requests
+app.all("/", (req, res) => {
+  res.status(200);
+  if (req.accepts("html")) {
+    return res.status(200).sendFile(path.join(__dirname, "views", "home.html"));
+  } else if (req.accepts("json")) {
+    return res.status(200).json({
+      message: "Hello from Express API",
+    });
+  } else {
+    return res.status(200).type("txt").send("Hello from Express API");
+  }
+});
 
 // Available routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, () => {
-  console.log(`iNotebook backend listening on port ${port}`);
+// If any other route except above routes, return responses mentioned below
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    return res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    return res.json({ error: "404 Not Found" });
+  } else {
+    return res.type("txt").send("404 Not Found");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`iNotebook backend listening on port ${PORT}`);
 });
